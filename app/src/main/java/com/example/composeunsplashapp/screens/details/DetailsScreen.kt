5d9ea4653d867842145ea2306c579246908d3b44
Nikita <nsalogub96@gmail.com>
2024-02-12 17:28:59 +0200

    fix scale for detail screen

--- app/src/main/java/com/example/composeunsplashapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/composeunsplashapp/screens/details/DetailsScreen.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -52,7 +53,7 @@ fun DetailsScreen(
     ) { innerPadding ->
         Surface(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .background(Color.White)
                 .padding(innerPadding)
         ) {
